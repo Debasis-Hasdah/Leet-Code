c9b2e88e1ea0679625d4a90b0f895f123bdb797f
Debasis-Hasdah <debasishasdah@gmail.com>
2025-12-02 22:35:32 +0530

    Make some changes in majority element

--- Array/MajorityElement.java
+++ Array/MajorityElement.java
@@ -1,8 +1,10 @@
+//majority element 
+
 import java.util.HashMap;
 
 public class MajorityElement {
     public static void main(String[] args) {
-        int[] nums1 = {3, 2, 3};
+        int[] nums1 = {3, 2, 3, 0};
         int[] nums2 = {2, 2, 1, 1, 1, 2, 2};
 
         Solution sol = new Solution();
