829903779da1176c92a7438e38adb48119538fb0
Debasis-Hasdah <debasishasdah@gmail.com>
2025-12-04 22:33:23 +0530

    solved add two number using Linked List problem

---
+++ LinkedList/AddTwoNumbers.java
@@ -0,0 +1,71 @@
+public class AddTwoNumbers {
+
+    // Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    // Solution class
+    public static class Solution {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode dummy = new ListNode(0);
+            ListNode current = dummy;
+            int carry = 0;
+
+            while (l1 != null || l2 != null || carry != 0) {
+                int sum = carry;
+
+                if (l1 != null) {
+                    sum += l1.val;
+                    l1 = l1.next;
+                }
+                if (l2 != null) {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+
+                carry = sum / 10;
+                current.next = new ListNode(sum % 10);
+                current = current.next;
+            }
+
+            return dummy.next;
+        }
+    }
+
+    // Main method (psvm)
+    public static void main(String[] args) {
+
+        // Creating first number: 342 -> 2 -> 4 -> 3
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        // Creating second number: 465 -> 5 -> 6 -> 4
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        Solution sol = new Solution();
+        ListNode result = sol.addTwoNumbers(l1, l2);
+
+        // Printing result
+        System.out.print("Result: ");
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
