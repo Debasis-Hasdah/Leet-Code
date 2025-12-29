4173f0a553a89d4c3b201dfaa1b60dd521b37c99
Debasis-Hasdah <debasishasdah@gmail.com>
2025-12-29 23:33:52 +0530

    make little  changes in the code

--- Array/FirstOccurrenceInString.java
+++ Array/FirstOccurrenceInString.java
@@ -1,6 +1,6 @@
 public class FirstOccurrenceInString {
 
-    // Method to find the first occurrence
+    
     public static int strStr(String haystack, String needle) {
         if (needle == null || needle.length() == 0) return 0;
         if (haystack == null) return -1;
@@ -8,7 +8,7 @@ public class FirstOccurrenceInString {
         int n = haystack.length();
         int m = needle.length();
 
-        // We only need to check positions up to n - m
+     
         for (int i = 0; i <= n - m; i++) {
             int j = 0;
             while (j < m && haystack.charAt(i + j) == needle.charAt(j)) {
@@ -19,9 +19,9 @@ public class FirstOccurrenceInString {
         return -1;
     }
 
-    // Main for testing
+  
     public static void main(String[] args) {
-        System.out.println(strStr("sadbutsad", "sad"));   // Output: 0
+        System.out.println(strStr("sadbutsad", "sadu"));   // Output: 0
         System.out.println(strStr("leetcode", "leeto"));  // Output: -1
         System.out.println(strStr("hello", "ll"));        // Output: 2
         System.out.println(strStr("aaaaa", "bba"));       // Output: -1
