ad234e54eb9f74acdc3904911233716f6d6279a5
Debasis-Hasdah <debasishasdah@gmail.com>
2026-01-28 23:32:18 +0530

    make some changes

--- Stringproblem/ExcelColumnTitle.java
+++ Stringproblem/ExcelColumnTitle.java
@@ -6,21 +6,21 @@ public class ExcelColumnTitle {
         StringBuilder sb = new StringBuilder();
 
         while (columnNumber > 0) {
-            columnNumber--;                         // shift down by 1
+            columnNumber--;                         
             int rem = columnNumber % 26;
             sb.append((char)('A' + rem));
             columnNumber /= 26;
         }
 
-        return sb.reverse().toString();           // reverse to get final
+        return sb.reverse().toString();           
     }
 
     public static void main(String[] args) {
-        System.out.println(convertToTitle(1));    // A
-        System.out.println(convertToTitle(28));   // AB
-        System.out.println(convertToTitle(701));  // ZY
-        System.out.println(convertToTitle(702));  // ZZ
-        System.out.println(convertToTitle(703));  // AAA
+        System.out.println(convertToTitle(1));   
+        System.out.println(convertToTitle(28));   
+        System.out.println(convertToTitle(701));  
+        System.out.println(convertToTitle(702));  
+        System.out.println(convertToTitle(703));  
     }
 }
 
