431b8f0121673d592c17de6db99f95daa8c1b591
Debasis-Hasdah <debasishasdah@gmail.com>
2026-01-31 00:53:40 +0530

    practic linkedlist

---
+++ Practice/BinarySearch.java
@@ -0,0 +1,10 @@
+public class BinarySearch {
+    // public static int binarySearch(int [] arr){
+
+    //     // int left=0;
+    //     // int right=arr.length-1;
+    //     //     while()
+
+    //     // return 0;
+    // }
+}
