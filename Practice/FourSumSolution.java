d34c8227dd773248489f007d4d6d3652dd4f3a4f
Debasis-Hasdah <debasishasdah@gmail.com>
2026-02-03 23:13:00 +0530

    solved 4 sum problem

---
+++ Practice/FourSumSolution.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class FourSumSolution {
+
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 4) return result;
+
+        Arrays.sort(nums);
+
+        int n = nums.length;
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+                        // skip duplicates
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 0, -1, 0, -2, 2};
+        int target1 = 0;
+        System.out.println("Input: " + Arrays.toString(nums1) + ", target = " + target1);
+        System.out.println("Output: " + fourSum(nums1, target1));
+
+        int[] nums2 = {2, 2, 2, 2, 2};
+        int target2 = 8;
+        System.out.println("\nInput: " + Arrays.toString(nums2) + ", target = " + target2);
+        System.out.println("Output: " + fourSum(nums2, target2));
+    }
+}
