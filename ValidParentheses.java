c117115ca5a756b76d9c4a4c643dfe30f9f5cd7c
Debasis-Hasdah <debasishasdah@gmail.com>
2026-02-08 00:31:45 +0530

    remove the commented line

--- ValidParentheses.java
+++ ValidParentheses.java
@@ -21,10 +21,10 @@ public class ValidParentheses {
     }
 
     public static void main(String[] args) {
-        System.out.println(isValid("()"));        // true
-        System.out.println(isValid("()[]{}"));    // true
-        System.out.println(isValid("(]"));        // false
-        System.out.println(isValid("([)]"));      // false
-        System.out.println(isValid("{[]}"));      // true
+        System.out.println(isValid("()"));        
+        System.out.println(isValid("()[]{}"));    
+        System.out.println(isValid("(]"));        
+        System.out.println(isValid("([)]"));      
+        System.out.println(isValid("{[]}"));      
     }
 }
