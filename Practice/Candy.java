7b3252dcae81cf9cb099f3a34ae3303758094625
Debasis-Hasdah <debasishasdah@gmail.com>
2026-02-11 23:51:33 +0530

    make some changes

--- Practice/Candy.java
+++ Practice/Candy.java
@@ -6,24 +6,24 @@ public class Candy {
         int n = ratings.length;
         int[] candies = new int[n];
 
-        // Give 1 candy to each child initially
+   
         Arrays.fill(candies, 1);
 
-        // Left to right
+        
         for (int i = 1; i < n; i++) {
             if (ratings[i] > ratings[i - 1]) {
                 candies[i] = candies[i - 1] + 1;
             }
         }
 
-        // Right to left
+        
         for (int i = n - 2; i >= 0; i--) {
             if (ratings[i] > ratings[i + 1]) {
                 candies[i] = Math.max(candies[i], candies[i + 1] + 1);
             }
         }
 
-        // Sum up
+       
         int total = 0;
         for (int c : candies) {
             total += c;
