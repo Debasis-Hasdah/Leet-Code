649e10dff885dc064c37685451f49a9955cc9c05
Debasis-Hasdah <debasishasdah@gmail.com>
2026-02-20 22:05:58 +0530

    solved generate parenthese problem

---
+++ Array/GenerateParenthesesPSVM.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class GenerateParenthesesPSVM {
+
+    public static void main(String[] args) {
+        int n = 3; // change this value to test
+        List<String> result = generateParenthesis(n);
+
+        System.out.println("Valid combinations:");
+        for (String s : result) {
+            System.out.println(s);
+        }
+    }
+
+    public static List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        backtrack(ans, "", 0, 0, n);
+        return ans;
+    }
+
+    private static void backtrack(List<String> ans, String current,
+                                  int open, int close, int n) {
+
+        // if string length = 2*n → valid combination
+        if (current.length() == 2 * n) {
+            ans.add(current);
+            return;
+        }
+
+        // add '(' if possible
+        if (open < n) {
+            backtrack(ans, current + "(", open + 1, close, n);
+        }
+
+        // add ')' if valid
+        if (close < open) {
+            backtrack(ans, current + ")", open, close + 1, n);
+        }
+    }
+}
