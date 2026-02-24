71d7765e71034fd0c3d54af87ca3d79b0862cd82
Debasis-Hasdah <debasishasdah@gmail.com>
2026-02-24 23:29:07 +0530

    removed commit lines

--- Array/GasStation.java
+++ Array/GasStation.java
@@ -11,9 +11,9 @@ public class GasStation {
 
     public static int canCompleteCircuit(int[] gas, int[] cost) {
 
-        int total = 0;      // total balance
-        int tank = 0;       // current tank
-        int start = 0;      // candidate start
+        int total = 0;      
+        int tank = 0;      
+        int start = 0;      
 
         for (int i = 0; i < gas.length; i++) {
             int diff = gas[i] - cost[i];
