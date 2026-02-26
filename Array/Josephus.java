dab541650cc42802e5f58eb11a14b3c6dd5b3722
Debasis-Hasdah <debasishasdah@gmail.com>
2026-02-26 23:42:35 +0530

    make some changes in codes

--- Array/Josephus.java
+++ Array/Josephus.java
@@ -1,8 +1,8 @@
 public class Josephus {
 
     public static void main(String[] args) {
-        int n = 7; // total people
-        int k = 3; // skip
+        int n = 7; 
+        int k = 3; 
 
         int winner = josephus(n, k);
 
@@ -10,12 +10,12 @@ public class Josephus {
     }
 
     public static int josephus(int n, int k) {
-        return josephusRec(n, k) + 1; // convert to 1-indexed
+        return josephusRec(n, k) + 1; 
     }
 
     private static int josephusRec(int n, int k) {
         if (n == 1) {
-            return 0; // base case
+            return 0; 
         }
         return (josephusRec(n - 1, k) + k) % n;
     }
