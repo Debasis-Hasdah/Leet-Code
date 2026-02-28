90497fca7ad3bbdc6296d8a46fe4e0cd22afc80a
Debasis-Hasdah <debasishasdah@gmail.com>
2026-03-01 01:11:34 +0530

    removed the unwanted comments

--- BigIntegerFactorial.java
+++ BigIntegerFactorial.java
@@ -9,7 +9,7 @@ public class BigIntegerFactorial {
         System.out.print("Enter a non-negative integer: ");
         int n = sc.nextInt();
 
-        // compute factorial using BigInteger
+       
         BigInteger fact = factorial(n);
 
         System.out.println(n + "! = " + fact);
@@ -17,10 +17,10 @@ public class BigIntegerFactorial {
     }
 
     public static BigInteger factorial(int n) {
-        // start from BigInteger.ONE
+        
         BigInteger result = BigInteger.ONE;
 
-        // multiply from 2 to n
+        
         for (int i = 2; i <= n; i++) {
             result = result.multiply(BigInteger.valueOf(i));
         }
