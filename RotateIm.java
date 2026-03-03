2695e4dd342d57d0f32add45f1c1b98c7b9cca08
Debasis-Hasdah <debasishasdah@gmail.com>
2026-03-03 23:27:51 +0530

    solved rotate imaage problem

---
+++ RotateIm.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+public class RotateIm {
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Before rotation:");
+        printMatrix(matrix);
+
+        rotate(matrix);
+
+        System.out.println("\nAfter rotation:");
+        printMatrix(matrix);
+    }
+
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // 1. Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // 2. Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+}
