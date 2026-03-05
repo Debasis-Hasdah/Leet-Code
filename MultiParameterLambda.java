a27d00c694a3c9d003605c632fff03f90a64c4e0
Debasis-Hasdah <debasishasdah@gmail.com>
2026-03-05 23:34:00 +0530

    solved multi parameter lamda problem

---
+++ MultiParameterLambda.java
@@ -0,0 +1,22 @@
+import java.util.function.BiFunction;
+
+public class MultiParameterLambda {
+
+    public static int helperFunction(int a, int b) {
+
+        // Lambda expression with two parameters
+        BiFunction<Integer, Integer, Integer> add = (x, y) -> x + y;
+
+        return add.apply(a, b);
+    }
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        int result = helperFunction(a, b);
+
+        System.out.println("Sum = " + result);
+    }
+}
