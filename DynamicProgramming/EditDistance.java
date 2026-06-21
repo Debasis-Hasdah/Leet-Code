41a27e218718082aa32eec83820f5647536fd5d5
Debasis-Hasdah <debasishasdah@gmail.com>
2026-06-21 22:30:17 +0530

    make some changes

--- DynamicProgramming/EditDistance.java
+++ DynamicProgramming/EditDistance.java
@@ -4,8 +4,8 @@ public class EditDistance {
 
     public static void main(String[] args) {
 
-        String word1 = "horse";
-        String word2 = "ros";
+        String word1 = "brush";
+        String word2 = "ush";
 
         int m = word1.length();
         int n = word2.length();
